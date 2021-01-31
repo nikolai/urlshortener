837a6a12cf00f96611f0ce22b039d01cfef72221
nikolai <nikolai.smirnov@gmail.com>
2021-01-31 13:43:44 +0300

    account service test

--- src/test/java/org/sng/shortener/services/AccountServiceTest.java
+++ src/test/java/org/sng/shortener/services/AccountServiceTest.java
@@ -10,15 +10,18 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 class AccountServiceTest {
 
     @Test
-    void register() throws AccountAlreadyExists {
+    void register() {
+        // теперь можно заменить внутренние ссылки на тестовые объекты!
         RandomGeneratorService rndGen = new RandomGeneratorService();
         AccountService accService = new AccountService(rndGen);
 
+        // теперь можно наполнить тестовые данные!
         String existAccId = "a153";
         Account existedAcc = new Account(existAccId, "secret");
         // условие выполнения теста (предусловие)
         accService.getRegisteredAccounts().put(existAccId, existedAcc);
 
+        // ожидаемое поведение - генерация исключения AccountAlreadyExists
         assertThatExceptionOfType(AccountAlreadyExists.class).isThrownBy(
             ()-> accService.register(existAccId)
         );
